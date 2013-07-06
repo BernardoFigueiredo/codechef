b28cb03e459ed1a6419347f8705612b3411ff6b7
Bernardo Figueiredo <bernardopascoalfigueiredo@gmail.com>
2013-07-07 00:31:28 +0100

    Finished FCTRL2. 0.14 5.7M

--- easy/FCTRL2.py
+++ easy/FCTRL2.py
@@ -4,7 +4,24 @@
 import array
 import sys
 
+a = array.array('i', (-1 for i in range(101)))
+a[1] = 1
+
+def fact(n):
+
+  if a[n] == -1:
+    return n * fact(n-1)
+
+  return a[n]
+  
+
+
 t = int(input())
-a = array.array('i', (-1 for i in range(100)))
+
+for i in range(t):
+  n = int(input())
+  print(fact(n))
+
+
 
 
